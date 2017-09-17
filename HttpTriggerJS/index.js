1285b720ef6a44e9330f4410e463dc6b57720db9
YOUR_NAME <YOUR_EMAIL>
2017-09-17 23:04:46 +0100

    retrieve todo items from db

--- HttpTriggerJS/index.js
+++ HttpTriggerJS/index.js
@@ -1,17 +1,22 @@
-var MongoClient = require('mongo').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
+const url = 'your connection string here';
 module.exports = function(context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
-
-  if (req.query.name || (req.body && req.body.name)) {
-    context.res = {
-      // status: 200, /* Defaults to 200 */
-      body: 'Hello there' + (req.query.name || req.body.name)
-    };
-  } else {
-    context.res = {
-      status: 400,
-      body: 'Please pass a name on the query string or in the request body'
-    };
-  }
-  context.done();
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    db
+      .collection('todo')
+      .find({})
+      .toArray(function(err, todos) {
+        if (err) throw err;
+        todos.forEach(t => {
+          delete t._id;
+        });
+        context.res = {
+          body: todos
+        };
+        db.close();
+        context.done();
+      });
+  });
 };
