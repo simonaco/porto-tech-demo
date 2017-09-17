b4cdced18f3feb33fd0a428a5183fcdafdba3475
YOUR_NAME <YOUR_EMAIL>
2017-09-17 23:13:29 +0100

    add insert todo logic

--- CreateTodo/index.js
+++ CreateTodo/index.js
@@ -1,17 +1,28 @@
-module.exports = function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-    context.done();
+var MongoClient = require('mongodb').MongoClient;
+const url = 'your connection string here';
+module.exports = function(context, req) {
+  context.log('JavaScript HTTP trigger function processed a request.');
+  if (req.body && req.body.todo) {
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      let todo = req.body.todo;
+      db
+        .collection('todo')
+        .insertOne({ id: todo.id, name: todo.name, desc: todo.desc })
+        .toArray(function(err, todos) {
+          if (err) throw err;
+          context.res = {
+            status: 200,
+            body: 'Todo Item inserted successfully!'
+          };
+          db.close();
+          context.done();
+        });
+    });
+  } else {
+    context.res = {
+      status: 400,
+      body: 'Please pass a todo in the request body'
+    };
+  }
 };
